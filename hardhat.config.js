0a784b0392289e9bc8bd4ebe2a64795cce8fab7b
Shashi <shashi3k3k@gmail.com>
2023-10-27 01:09:26 +0530

    Completed with the blockchain-lottery-smart contract

---
+++ hardhat.config.js
@@ -0,0 +1,18 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.19",
+  networks:{
+    goerli:{
+      url:process.env.RPC_URL,
+      accounts:[process.env.PRIVATE_KEY]
+    }
+  },
+  etherscan:{
+    apiKey:{
+      goerli:"X61J76RE4BN6DCVH5RSXDQTST81ZRNUDXJ"
+    }
+  }
+};
